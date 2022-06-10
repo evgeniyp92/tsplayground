0109c435ceadb75d5e0c48a13465371abe385a5b
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-06-10 08:46:51 +0200

    refactored matchreader.ts

--- Projects/3-stats/src/MatchReader.ts
+++ Projects/3-stats/src/MatchReader.ts
@@ -5,22 +5,27 @@ type MatchData = [Date, string, string, number, number, MatchResult, string];
 
 interface DataReader {
   read(): void;
-  data: string[];
+  data: string[][];
 }
 
 export class MatchReader {
-  constructor(public reader: DataReader) {
-    // .map((row: string[]): MatchData => {
-    //     return [
-    //       dateStringToDate(row[0]),
-    //       row[1],
-    //       row[2],
-    //       parseInt(row[3]),
-    //       parseInt(row[4]),
-    //       // asserting that row[5] is of type MatchResult
-    //       row[5] as MatchResult,
-    //       row[6],
-    //     ];
-    //   });
+  matches: MatchData[] = [];
+
+  constructor(public reader: DataReader) {}
+
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map((row: string[]): MatchData => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        // asserting that row[5] is of type MatchResult
+        row[5] as MatchResult,
+        row[6],
+      ];
+    });
   }
 }
