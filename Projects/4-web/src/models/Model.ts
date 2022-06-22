d787682e710211ebdb086ca142d8e6257855f433
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-06-22 13:45:03 +0200

    finished generic model refactor

--- Projects/4-web/src/models/Model.ts
+++ Projects/4-web/src/models/Model.ts
@@ -1,4 +1,4 @@
-import { AxiosPromise } from 'axios';
+import { AxiosError, AxiosPromise, AxiosResponse } from 'axios';
 
 interface ModelAttributes<Type> {
   set(update: Type): void;
@@ -16,4 +16,59 @@ interface Events {
   trigger(eventName: string): void;
 }
 
-export class Model {}
+interface HasID {
+  id?: number;
+}
+
+export class Model<Type extends HasID> {
+  constructor(
+    public attributes: ModelAttributes<Type>,
+    public events: Events,
+    public sync: Sync<Type>
+  ) {}
+
+  // PASSTHRU METHODS ----------------------------------------------------------
+  // bad code
+  // public on(eventName: string, callback: Callback) {
+  //   this.events.on(eventName, callback);
+  // }
+
+  // good code
+  // think of it as a forwarder to the actual on function
+  public get on() {
+    return this.events.on;
+  }
+
+  public get trigger() {
+    return this.events.trigger;
+  }
+
+  public get get() {
+    return this.attributes.get;
+  }
+
+  // COORDINATION REQUIRED -----------------------------------------------------
+  public set(update: Type): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  public fetch(): void {
+    const id = this.get('id');
+    if (typeof id !== 'number') throw new Error('Cannot fetch without an id');
+    this.sync.fetch(id).then((r: AxiosResponse): void => {
+      this.set(r.data);
+    });
+  }
+
+  public save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((r: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch((e: AxiosError) => {
+        this.trigger('errror');
+      });
+  }
+}
--- Projects/4-web/src/models/User.ts
+++ Projects/4-web/src/models/User.ts
@@ -1,7 +1,7 @@
-import { Eventing, Callback } from './Eventing';
-import { Sync } from './Sync';
+import { Model } from './Model';
 import { Attributes } from './Attributes';
-import { AxiosError, AxiosResponse } from 'axios';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
 
 export interface UserProps {
   id?: number;
@@ -11,57 +11,12 @@ export interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-  private events: Eventing = new Eventing();
-  private sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  private attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-
-  // PASSTHRU METHODS ----------------------------------------------------------
-  // bad code
-  // public on(eventName: string, callback: Callback) {
-  //   this.events.on(eventName, callback);
-  // }
-
-  // good code
-  // think of it as a forwarder to the actual on function
-  public get on() {
-    return this.events.on;
-  }
-
-  public get trigger() {
-    return this.events.trigger;
-  }
-
-  public get get() {
-    return this.attributes.get;
-  }
-
-  // COORDINATION REQUIRED -----------------------------------------------------
-  public set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change');
-  }
-
-  public fetch(): void {
-    const id = this.get('id');
-    if (typeof id !== 'number') throw new Error('Cannot fetch without an id');
-    this.sync.fetch(id).then((r: AxiosResponse): void => {
-      this.set(r.data);
-    });
-  }
-
-  public save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((r: AxiosResponse): void => {
-        this.trigger('save');
-      })
-      .catch((e: AxiosError) => {
-        this.trigger('errror');
-      });
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps) {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
   }
 }
