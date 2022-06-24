962ced5476eae483b102a7774338b62ddb5096cc
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-06-24 09:00:54 +0200

    added user property rendering

--- Projects/4-web/src/index.ts
+++ Projects/4-web/src/index.ts
@@ -1,5 +1,11 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root') as HTMLElement);
+const user = User.buildUser({ name: 'STEVE', age: 20 });
+
+const userForm = new UserForm(
+  document.getElementById('root') as HTMLElement,
+  user
+);
 
 userForm.render();
