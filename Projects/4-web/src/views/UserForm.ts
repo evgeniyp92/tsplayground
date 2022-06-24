cdcada4f6f7702eac9267a8419942bf651c90add
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-06-24 08:17:28 +0200

    init html rendering class

---
+++ Projects/4-web/src/views/UserForm.ts
@@ -0,0 +1,19 @@
+export class UserForm {
+  constructor(public parent: Element) {}
+
+  // setting up the base template
+  private template(): string {
+    return `
+			<div>
+				<h1>User Form</h1>
+				<input />
+			</div>
+		`;
+  }
+
+  public render(): void {
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+    this.parent.append(templateElement.content);
+  }
+}
