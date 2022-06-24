46e695ca21692db18fa066eb6b65e86a3fd05857
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-06-24 09:21:11 +0200

    fixed form elements stacking

--- Projects/4-web/src/views/UserForm.ts
+++ Projects/4-web/src/views/UserForm.ts
@@ -50,6 +50,7 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = '';
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
     this.bindEvents(templateElement.content);
