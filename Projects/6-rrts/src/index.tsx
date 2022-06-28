549dd99c480165df75e43292ac222c09b7ed8e2b
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-06-28 14:19:46 +0200

    wrote simple page

--- Projects/6-rrts/src/index.tsx
+++ Projects/6-rrts/src/index.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Component } from 'react';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Always describe the structure of props with an interface above the component
+interface AppProps {
+  color?: string;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component<AppProps> {
+  render() {
+    return <div>{this.props.color}</div>;
+  }
+}
+
+const container = document.getElementById('root');
+if (container) {
+  createRoot(container).render(<App />);
+}
