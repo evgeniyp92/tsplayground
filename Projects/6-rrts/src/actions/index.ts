f4246de6814ac2d9be4fecb2a7f45599acff9a27
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-06-29 10:27:44 +0200

    added dispatch interface

--- Projects/6-rrts/src/actions/index.ts
+++ Projects/6-rrts/src/actions/index.ts
@@ -9,13 +9,20 @@ interface Todo {
   completed: boolean;
 }
 
+// dispatch interface
+interface FetchTodosAction {
+  type: ActionTypes.FetchTodos;
+  payload: Todo[];
+}
+
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     // telling axios what to expect to get back
     const res = await axios.get<Todo[]>(
       'http://jsonplaceholder.typicode.com/todos'
     );
-    dispatch({
+    // limiting what can be dispatched within this scope
+    dispatch<FetchTodosAction>({
       type: ActionTypes.FetchTodos,
       payload: res.data,
     });
