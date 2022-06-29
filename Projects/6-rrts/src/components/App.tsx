f22938c57a3f0c05100352386ff64ed6da5b9e3c
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-06-29 09:42:48 +0200

    set up core redux integration

---
+++ Projects/6-rrts/src/components/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export class App extends Component {
+  render() {
+    return <div>Hi there</div>;
+  }
+}
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- Projects/6-rrts/src/index.tsx
+++ Projects/6-rrts/src/index.tsx
@@ -1,53 +1,19 @@
-import { Component } from 'react';
 import { createRoot } from 'react-dom/client';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-// Always describe the structure of props with an interface above the component
-// You have to override the default, readonly empty objects
-interface AppProps {
-  color?: string;
-}
+import App from './components/App';
 
-// Would have been passed as the second generic
-// interface AppState {
-//   counter: number;
-// }
+import { reducers } from './reducers';
 
-// example of a functional component
-// const App = (props: AppProps): JSX.Element => {
-//   return <div>{props.color}</div>;
-// };
-
-class App extends Component<AppProps> {
-  // annoying, finicky way of setting up state
-  // constructor(props: AppProps) {
-  //   super(props);
-
-  //   this.state = { counter: 0 };
-  // }
-
-  // simpler way of setting up state, via overriding
-  state = { counter: 0 };
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const container = document.getElementById('root');
 if (container) {
-  createRoot(container).render(<App color="red" />);
+  createRoot(container).render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 }
