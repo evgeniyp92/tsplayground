4cbd33d900a623b84a7da0b05a1f376e36b163ed
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-06-29 11:29:41 +0200

    refactor actions

---
+++ Projects/6-rrts/src/actions/todos.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+
+// setting up a response interface
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+// dispatch interface
+export interface FetchTodosAction {
+  type: ActionTypes.FetchTodos;
+  payload: Todo[];
+}
+
+export interface DeleteTodoAction {
+  type: ActionTypes.DeleteTodo;
+  payload: number;
+}
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    // telling axios what to expect to get back
+    const res = await axios.get<Todo[]>(
+      'http://jsonplaceholder.typicode.com/todos'
+    );
+    // limiting what can be dispatched within this scope
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.FetchTodos,
+      payload: res.data,
+    });
+  };
+};
+
+export const deleteTodo = (id: number): DeleteTodoAction => {
+  return {
+    type: ActionTypes.DeleteTodo,
+    payload: id,
+  };
+};
