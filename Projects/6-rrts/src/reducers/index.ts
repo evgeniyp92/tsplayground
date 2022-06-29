43935cb20761124e533eca49683b8522a181f1f9
Evgeniy Pimenov <1@evgeniypimenov.com>
2022-06-29 10:55:03 +0200

    write rdcrs index and interface to control returns

--- Projects/6-rrts/src/reducers/index.ts
+++ Projects/6-rrts/src/reducers/index.ts
@@ -1,6 +1,13 @@
 import { combineReducers } from 'redux';
-export const reducers = combineReducers({
-  counter: () => {
-    return 1;
-  },
+import { todosReducer } from './todos';
+import { Todo } from '../actions';
+
+export interface StoreState {
+  todos: Todo[];
+}
+
+// export type StoreState = ReturnType<typeof rootReducer>;
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer,
 });
--- Projects/6-rrts/src/reducers/todos.ts
+++ Projects/6-rrts/src/reducers/todos.ts
@@ -1,7 +1,7 @@
 import { FetchTodosAction, Todo } from '../actions';
 import { ActionTypes } from '../actions/types';
 
-export const todosReducer = (action: FetchTodosAction, state: Todo[] = []) => {
+export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
   switch (action.type) {
     case ActionTypes.FetchTodos:
       return action.payload;
